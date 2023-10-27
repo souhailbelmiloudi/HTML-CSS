c3bd353e327b8149d37ecd5582dd06d45cddef91
souhail <souhailbelmiloudi@gmail.com>
2023-10-27 20:36:57 +0200

    responsive

--- Portofolio/js/index.js
+++ Portofolio/js/index.js
@@ -1,4 +1,4 @@
-//============toggele icon navbar============
+//=============toggele icon navbar============= 
 const menuIcon =document.querySelector("#menu-icon")
 const navBar =document.querySelector(".navbar")
 
@@ -7,3 +7,38 @@ menuIcon.addEventListener("click", ()=>{
     navBar.classList.toggle('active')
 
 })
+/**=============scroll section active link============= */
+let sections =document.querySelectorAll("section")
+let navlinks =document.querySelectorAll("header nav a")
+window.addEventListener("scroll" ,()=>{
+   
+   
+    sections.forEach(section=>{
+        let top =window.scrollY;
+        let offset=section.offsetTop-150
+        let height =section.offsetHeight
+        let id= section.getAttribute("id")
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(link =>{
+                link.classList.remove('active');
+                document.querySelector("header nav a[href*="  +id+ "]").classList.add('active');
+            })
+        }
+    })
+
+/**============= sticky navbar =============  */
+
+    let header =document.querySelector("header")
+
+    header.classList.toggle('sticky' ,window.scrollY >100)
+
+    menuIcon.classList.remove('bx-x')
+    navBar.classList.remove('active')
+})
+
+
+
+
+
+//============ slider==============
+
